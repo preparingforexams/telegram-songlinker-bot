e589debfa6e1496849936aa80b86a4645be15d39
Björn Petersen <git@bjoernpetersen.net>
2022-06-23 17:21:44 +0200

    Try with more args

--- src/songlinker/slack/app.py
+++ src/songlinker/slack/app.py
@@ -11,15 +11,18 @@ class SlackApp:
         self.port = int(os.getenv("SLACK_PORT"))
         signing_secret = os.getenv("SLACK_SIGNING_SECRET")
         token = os.getenv("SLACK_TOKEN")
+
         app = App(
             signing_secret=signing_secret,
             token=token,
         )
         self.app = app
+
         app.event("link_shared")(self.on_link_shared)
 
     def run(self):
         self.app.start(port=self.port, path="/")
 
-    def on_link_shared(self, event):
+    def on_link_shared(self, client, event, ack, say):
         _LOG.info("Received event: %s", event)
+        ack()
