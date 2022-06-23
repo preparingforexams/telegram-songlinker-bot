ff52af521331ca8373716d503528c98e45a27584
Björn Petersen <git@bjoernpetersen.net>
2022-06-23 17:18:43 +0200

    'Handle' link_shared events

--- src/songlinker/slack/app.py
+++ src/songlinker/slack/app.py
@@ -1,6 +1,9 @@
+import logging
 import os
 
 from slack_bolt import App
+
+_LOG = logging.getLogger(__name__)
 
 
 class SlackApp:
@@ -8,11 +11,15 @@ class SlackApp:
         self.port = int(os.getenv("SLACK_PORT"))
         signing_secret = os.getenv("SLACK_SIGNING_SECRET")
         token = os.getenv("SLACK_TOKEN")
-        self.app = App(
+        app = App(
             signing_secret=signing_secret,
             token=token,
         )
+        self.app = app
+        app.event("link_shared")(self.on_link_shared)
 
     def run(self):
-        # TODO: implement
         self.app.start(port=self.port, path="/")
+
+    def on_link_shared(self, event):
+        _LOG.info("Received event: %s", event)
