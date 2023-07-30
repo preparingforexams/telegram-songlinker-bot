169561d51362b2bac183356b14502ea02033ee55
Björn Petersen <git@bjoernpetersen.net>
2023-07-30 12:06:09 +0200

    Add test for full response

--- src/songlinker/link_api.py
+++ src/songlinker/link_api.py
@@ -1,10 +1,35 @@
 from typing import Self
 
 import httpx
-from pydantic import BaseModel
+from pydantic import BaseModel, ConfigDict, HttpUrl
+from pydantic.alias_generators import to_camel
 
 
-class LinkResponse(BaseModel):
+class CamelCaseModel(BaseModel):
+    model_config = ConfigDict(alias_generator=to_camel)
+
+
+class PlatformLink(CamelCaseModel):
+    url: HttpUrl
+    entity_unique_id: str
+
+
+class LinkResponse(CamelCaseModel):
+    page_url: HttpUrl
+    links_by_platform: dict[str, PlatformLink]
+
+
+class SongLinks(BaseModel):
+    page: HttpUrl
+    apple_music: HttpUrl | None
+    deezer: HttpUrl | None
+    spotify: HttpUrl | None
+    soundcloud: HttpUrl | None
+    tidal: HttpUrl | None
+    youtube: HttpUrl | None
+
+
+class IoException(Exception):
     pass
 
 
@@ -13,7 +38,7 @@ class LinkApi:
 
     def __init__(self, api_key: str):
         self._api_key = api_key
-        self._client = httpx.Client()
+        self._client = httpx.Client(timeout=20)
 
     def __enter__(self) -> Self:
         return self
@@ -23,3 +48,49 @@ class LinkApi:
 
     def close(self):
         self._client.close()
+
+    def _extract_url(
+        self,
+        links_by_platform: dict[str, PlatformLink],
+        platform: str,
+    ) -> HttpUrl | None:
+        platform_link = links_by_platform.get(platform)
+        if platform_link is None:
+            return None
+        return platform_link.url
+
+    def _parse_response(self, content: bytes) -> SongLinks:
+        response = LinkResponse.model_validate_json(content)
+        result = SongLinks(
+            page=response.page_url,
+            apple_music=self._extract_url(response.links_by_platform, "appleMusic"),
+            deezer=self._extract_url(response.links_by_platform, "deezer"),
+            spotify=self._extract_url(response.links_by_platform, "spotify"),
+            soundcloud=self._extract_url(response.links_by_platform, "soundcloud"),
+            tidal=self._extract_url(response.links_by_platform, "tidal"),
+            youtube=self._extract_url(response.links_by_platform, "youtube"),
+        )
+        return SongLinks.model_validate(result)
+
+    def lookup_links(self, url: str) -> SongLinks | None:
+        try:
+            response = self._client.get(
+                url=self.BASE_URL,
+                params={
+                    "url": url,
+                    "userCountry": "DE",
+                    "key": self._api_key,
+                },
+            )
+        except httpx.RequestError as e:
+            raise IoException from e
+
+        status_code = response.status_code
+        if response.is_success:
+            return self._parse_response(response.content)
+        elif 400 <= status_code < 500:
+            raise IoException(f"Client error during request: {status_code}")
+        elif 500 <= status_code < 600:
+            raise IoException(f"Received server error {status_code}")
+        else:
+            raise IoException(f"Unexpected response status: {status_code}")
---
+++ src/tests/link_api/test_link_api.py
@@ -0,0 +1,53 @@
+import os
+from unittest.mock import MagicMock
+
+import pytest
+import vcr
+
+from songlinker.link_api import LinkApi
+from tests.conftest import require_integration
+
+
+@pytest.fixture()
+def api_token(require_integration) -> str:
+    result = os.getenv("SONGLINK_API_TOKEN")
+    assert result
+    return result
+
+
+@pytest.fixture()
+def link_api(api_token) -> LinkApi:
+    with LinkApi(api_key=api_token) as api:
+        yield api
+
+
+def test_context_manager__works(mocker):
+    close: MagicMock = mocker.spy(LinkApi, "close")
+
+    with LinkApi(api_key="fake") as api:
+        assert isinstance(api, LinkApi)
+        close.assert_not_called()
+
+    close.assert_called_once()
+
+
+@pytest.mark.integration
+@pytest.mark.parametrize(
+    "url",
+    [
+        "https://open.spotify.com/track/0SZemtszoaQHL3aUuMS6WF?si=110f087282bd457f",
+        "https://open.spotify.com/track/0d28khcov6AiegSCpG5TuT?si=49a8f03c107746d4",
+        "https://www.youtube.com/watch?v=fJ9rUzIMcZQ&pp=ygUGcXVlZW4g",
+    ],
+)
+@vcr.use_cassette(record_mode="new_episodes")
+def test_lookup_links_has_all_links(link_api, url):
+    response = link_api.lookup_links(url)
+    assert response is not None
+
+    assert response.apple_music
+    assert response.deezer
+    assert response.spotify
+    assert response.soundcloud
+    assert response.tidal
+    assert response.youtube
