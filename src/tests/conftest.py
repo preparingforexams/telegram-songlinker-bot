0336248a9b777e72b6f54a646cf0e032bbcbeee4
Björn Petersen <git@bjoernpetersen.net>
2023-07-30 10:46:30 +0200

    Make sure integration tests are properly marked

---
+++ src/tests/conftest.py
@@ -0,0 +1,8 @@
+import pytest
+
+
+@pytest.fixture()
+def require_integration(request: pytest.FixtureRequest) -> None:
+    marker = request.node.get_closest_marker("integration")
+    if marker is None:
+        raise pytest.UsageError(f"Fixture is only available in integration tests")
--- src/tests/test_link_api.py
+++ src/tests/test_link_api.py
@@ -1,6 +1,23 @@
+import os
 from unittest.mock import MagicMock
 
+import pytest
+
 from songlinker.link_api import LinkApi
+from tests.conftest import require_integration
+
+
+@pytest.fixture()
+def api_token(require_integration) -> str:
+    result = os.getenv("SONGLINK_API_TOKEN")
+    assert result
+    return result
+
+
+@pytest.fixture()
+def link_api(api_token) -> LinkApi:
+    with LinkApi(api_key=api_token) as api:
+        yield api
 
 
 def test_context_manager__works(mocker):
