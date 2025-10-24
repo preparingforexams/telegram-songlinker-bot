f038e2c76e22eb5bf140b4115e4d5f30464691bd
Björn Petersen <git@bjoernpetersen.net>
2025-10-24 13:30:05 +0200

    chore: update to bs-config 3

--- Makefile
+++ Makefile
@@ -3,19 +3,19 @@ check: lint test
 
 .PHONY: lint
 lint:
-	uv run ruff format src/
-	uv run ruff check --fix --show-fixes src/
+	uv run ruff format
+	uv run ruff check --fix --show-fixes
 	uv run mypy src/
 
 .PHONY: test
 test:
-	uv run pytest --record-mode=new_episodes src/
+	uv run pytest --record-mode=new_episodes
 
 .PHONY: unit-test
 unit-test:
-	uv run pytest -m "not integration" src/
+	uv run pytest -m "not integration"
 
 .PHONY: integration-test
 integration-test:
-	uv run pytest -m "integration" --record-mode=new_episodes src/
+	uv run pytest -m "integration" --record-mode=new_episodes
 
--- src/songlinker/config.py
+++ src/songlinker/config.py
@@ -19,10 +19,10 @@ class Config:
     @classmethod
     def from_env(cls, env: Env) -> Self:
         return cls(
-            app_version=env.get_string("APP_VERSION", default="dirty"),
-            nats=NatsConfig.from_env(env.scoped("NATS_")),
-            telegram_api_key=env.get_string("TELEGRAM_TOKEN", required=True),
-            songlinker_api_key=env.get_string("SONGLINK_API_TOKEN", required=True),
-            sentry_dsn=env.get_string("SENTRY_DSN"),
-            enable_telemetry=env.get_bool("ENABLE_TELEMETRY", default=False),
+            app_version=env.get_string("app-version", default="dirty"),
+            nats=NatsConfig.from_env(env / "nats"),
+            telegram_api_key=env.get_string("telegram-token", required=True),
+            songlinker_api_key=env.get_string("songlink-api-token", required=True),
+            sentry_dsn=env.get_string("sentry-dsn"),
+            enable_telemetry=env.get_bool("enable-telemetry", default=False),
         )
